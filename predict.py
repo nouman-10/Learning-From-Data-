70c6654531f8d9f56b30386c391579cb9132492d
Nouman Ahmed <35970677+nouman-10@users.noreply.github.com>
2021-11-08 16:11:10 +0100

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,60 @@
+from model import BaselineModel, identity
+
+import argparse
+import pickle
+import os
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--data_dir",
+        default="./data/",
+        type=str,
+        help="Input directory to read json files from (default ./data/)",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_path",
+        default="./output/",
+        type=str,
+        help="Output directory to save the results to (default ./output/)",
+    )
+    parser.add_argument(
+        "-m",
+        "--saved_model_path",
+        default="./data/best_baseline_model.pkl",
+        type=str,
+        help="Read saved model from the path (default ./data/best_baseline_model.pkl)",
+    )
+    args = parser.parse_args()
+    return args
+
+def load_model(path):
+    with open(path, "rb") as f:
+        return pickle.load(f)    
+
+def read_labels(path):
+    with open(path, "r") as f:
+        return f.read().split('\n')
+        
+if __name__ == "__main__":
+    args = create_arg_parser()
+
+    # Test the best saved model on the test file
+    model = BaselineModel(
+        data_path=args.data_dir,
+        model_type="svm",
+        model_params={"kernel": "linear"},
+        vectorizer_type="tfidf",
+        vectorizer_params={"max_df": 0.95, "max_features": 50000, "ngram_range": (1, 2)},
+        extra_feature=None,
+    )
+    model.labels = read_labels(os.path.join(args.data_dir, "labels.txt"))
+    best_model = load_model(args.saved_model_path)
+    model.pipeline = best_model
+    model.read_data(only_test=True)
+    model.evaluate()
+    model.print_results()
+    model.save_results(os.path.join(args.output_path, "test_results.txt"))
+
